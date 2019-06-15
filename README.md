21f88e6741c765f250b39b835010f97107de1a2c
ckaoki <cullan_aoki@msn.com>
2019-06-14 20:21:26 -0700

    completed algorithm to display train arrival times and minutes until arrival. Composed readme file.

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # TrainScheduler
+This project contains a web page that allows the user to view and create train schedules. This page was designed using Bootstrap and is responsive.
+
+Overview
+1. The page displays train schedules that include the train name, destination, frequency, next arrival, and minutes away.
+2. Train schedules are stored on a firebase database. Specifically the data for train name, destination, frequency, and first train time are stored. 
+3. Next train arrival and minutes away are calculate.
+4. Users are allowed to create new train schedules which are immediately displayed upon submission.
+5. Train schedules can be view on multiple browsers simultaneously.
+6. Due to lack of funding the browser refresh button must be pressed to get current train arrival times.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+
+### Prerequisites
+
+Browser minimum requirements:
+* Chrome ver 74
+* Firefox ver 60
+
+### Installing
+
+To view files navigate to  https://ckaoki.github.io/TrainScheduler/.
+
+To install source files use git to clone files from https://github.com/ckaoki/TrainScheduler.
+
+The app.js file contains the trivia game code.
+
+## Running the tests
+
+Open files in browser and make sure they are properly formatted. Your eyes are the test.
+
+### Break down into end to end tests
+
+Source files have been extensively tested by displaying pages in Google Chrome and using visual verification.
+
+### And coding style tests
+
+I'm starting to get some style.
+
+## Deployment
+
+Navigate to https://github.com/ckaoki/TrainScheduler to clone.
+
+## Built With
+
+* [Visual Studio Code](https://code.visualstudio.com/)
+
+## Contributing
+
+Please read our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use github for version control (https://github.com/your/project/tags). 
+
+## In development
+Implementing textual hints.
+
+## Authors
+
+* **Cullan Aoki** - *Initial work* - https://github.com/ckaoki/TrainScheduler
+
+## License
+
+This project is not licensed.
+
+## Acknowledgments
+
+* Joe Rehfuss
+* Trae Shanks
+* Lan Truong
+* James Hanley
--- assets/javascript/trainScheduler.js
+++ assets/javascript/trainScheduler.js
@@ -11,7 +11,6 @@ appId: "1:485330656695:web:8cc4f4d0a7bbf6a7"
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 var database = firebase.database();
 var connectionRef = database.ref("/connections");
 var connectedRef = database.ref(".info/connected");
@@ -19,8 +18,7 @@ var connectedRef = database.ref(".info/connected");
 
 //   A $( document ).ready() block.
 $( document ).ready(function() {
-    console.log( "ready!" )  
-    
+    console.log( "ready!" )      
 });
 
 
@@ -32,8 +30,6 @@ $(document).on("click","#submitButton", function(event){
     var destination = $("#destination").val().trim();
     var firstTrainTime = $("#firstTrainTime").val().trim();
     var frequency = $("#frequency").val().trim();
-    console.log(firstTrainTime);
-
 
     // push values to firebase
     database.ref().push({
@@ -46,35 +42,62 @@ $(document).on("click","#submitButton", function(event){
 // firebase event for new train schedule.
 // Updates page.
 database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
-
+    //console.log(childSnapshot.val());
     // get values from firebase
     var trainName = childSnapshot.val().trainName;
     var destination = childSnapshot.val().destination;
     var firstTrainTime = childSnapshot.val().firstTrainTime;
     var frequency = childSnapshot.val().frequency;
+    var nextArrival = "";
+    var minutesAway = "";
 
     // perform calculations on Next Arrival and Minutes Away fields
-
-
-    // create columns with values on page
-    var colTrainName = $("<td>");
-    var colDestination = $("<td>");
-    var colFrequency = $("<td>");
-    var colNextArrival = $("<td>");
-    var colMinutesAway = $("<td>");
+    var arrayFTT = firstTrainTime.split(":");
+    var hrs = arrayFTT[0];
+    var mins = arrayFTT[1];
+    var now = moment();
+    var momFirstTrainTime = moment();
+    momFirstTrainTime.set({hour:parseInt(hrs),minute:parseInt(mins),second:0,millisecond:0});
+    momFirstTrainTime.toISOString()
+    momFirstTrainTime.format()
+    var minutesNowMinusFirst = now.diff(momFirstTrainTime);
+    var minutesNowMinusFirst = moment.duration(minutesNowMinusFirst).asMinutes();
     
-    colTrainName.text(trainName);
+    // train arrival logic
+    if(minutesNowMinusFirst <= 0){
+        // first train hasn't come yet
+        nextArrival = momFirstTrainTime.format("HH:mm");
+        minutesAway = Math.abs(Math.floor(minutesNowMinusFirst));
+    }
+    else{
+        // all other train intervals
+        var minutes = minutesNowMinusFirst % frequency;
+        var hours = parseInt(minutesNowMinusFirst / frequency);
+        nextArrival = momFirstTrainTime.add(frequency*(hours+1), "minutes");
+        minutesAway = Math.ceil(moment.duration(nextArrival.diff(now)).asMinutes()); 
+        nextArrival = nextArrival.format("HH:mm");    
+    }
+   
+    // create columns with values on page
+    var newTrainName = $("<td>");
+    var newDestination = $("<td>");
+    var newFrequency = $("<td>");
+    var newNextArrival = $("<td>");
+    var newMinutesAway = $("<td>");
+    
+    newTrainName.text(trainName);
     console.log(trainName);
-    colDestination.text(destination);
-    colFrequency.text(frequency);
+    newDestination.text(destination);
+    newFrequency.text(frequency);
+    newNextArrival.text(nextArrival);
+    newMinutesAway.text(minutesAway);
 
     // dipslay values on page
     var newRow = $("<tr>");
-    newRow.append(colTrainName);
-    newRow.append(colDestination);
-    newRow.append(colFrequency);
-    newRow.append(colNextArrival);
-    newRow.append(colMinutesAway);
+    newRow.append(newTrainName);
+    newRow.append(newDestination);
+    newRow.append(newFrequency);
+    newRow.append(newNextArrival);
+    newRow.append(newMinutesAway);
     $("#currentTrainSchedule > tbody").append(newRow);
 });
